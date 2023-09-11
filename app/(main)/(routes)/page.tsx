342a829b3acb17298ea64df3e17fc2d9a1baea74
numdinkushi <numdinkushi@gmail.com>
2023-09-11 17:34:41 +0100

    added dark and light mode theme

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,3 +1,4 @@
+import { ModeToggle } from "@/components/mode.toggle";
 import { cn } from "@/lib/utils";
 import { UserButton } from "@clerk/nextjs";
 
@@ -7,6 +8,7 @@ export default function Home() {
   return (
     <div className="">
       <UserButton afterSignOutUrl="/"/>
+      <ModeToggle />
     </div>
   );
 }
