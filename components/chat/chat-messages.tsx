401306090225eab64d566adc2fef3b99ba281574
numdinkushi <numdinkushi@gmail.com>
2023-09-18 14:48:23 +0100

    can edit message

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -5,37 +5,41 @@ import ChatWelcome from "./chat-welcome";
 import { useChatQuery } from "@/hooks/use-chat-query";
 import { Loader2, ServerCrash } from "lucide-react";
 import { Fragment } from "react";
+import ChatItem from "./chat-item";
+import { format } from "date-fns";
+
+const DATE_FORMAT = "d MMM yyy, HH:mm";
 
 type MessageWithMemberWithProfile = Message & {
   member: Member & {
-    profile: Profile
-  }
-}
+    profile: Profile;
+  };
+};
 
 interface ChatMessagesProps {
-    name: string;
-    member: Member;
-    chatId: string;
-    apiUrl: string;
-    socketUrl: string;
-    socketQuery:  Record<string, string>;
-    paramKey: "channelId" | "conversationId";
-    paramValue: string;
-    type: "channel" | "conversation";
+  name: string;
+  member: Member;
+  chatId: string;
+  apiUrl: string;
+  socketUrl: string;
+  socketQuery: Record<string, string>;
+  paramKey: "channelId" | "conversationId";
+  paramValue: string;
+  type: "channel" | "conversation";
 }
 
 const ChatMessages = ({
-    name,
-    type,
-     member,
-     chatId,
-     apiUrl,
-     socketUrl,
-     socketQuery,
-     paramKey,
-     paramValue
-} : ChatMessagesProps) => {  
-  const queryKey = `chat:${chatId}`
+  name,
+  type,
+  member,
+  chatId,
+  apiUrl,
+  socketUrl,
+  socketQuery,
+  paramKey,
+  paramValue
+}: ChatMessagesProps) => {
+  const queryKey = `chat:${chatId}`;
   const {
     data,
     fetchNextPage,
@@ -48,14 +52,14 @@ const ChatMessages = ({
     paramKey,
     paramValue
   });
-
+  console.log(555, data?.pages[0]['items'][0]['fileUrl']);
   if (status === 'loading') {
     return (
       <div className="flex flex-col flex-1 justify-center items-center">
         <Loader2 className="h-7 w-7 text-zinc-500 animate-spin my-4" />
         <p className="text-xs text-zinc-500 dark:text-zinc-400">Loading messages...</p>
       </div>
-    )
+    );
   }
 
   if (status === 'error') {
@@ -64,28 +68,38 @@ const ChatMessages = ({
         <ServerCrash className="h-7 w-7 text-zinc-500 my-4" />
         <p className="text-xs text-zinc-500 dark:text-zinc-400">Something went wrong</p>
       </div>
-    )
+    );
   }
   return (
     <div className=" flex flex-1 flex-col py-4 overflow-y-auto">
-        <div className="flex-1 "></div>
-        <ChatWelcome
-            type={type}
-            name={name}
-        />
-        <div className="flex flex-col-reverse mt-auto">
-          {data?.pages?.map((group, index) => (
-            <Fragment key={index}>
-              {group.items.map((message: MessageWithMemberWithProfile) => (
-                <div key={message.id} className="">
-                  {message.content}
-                </div>
-              ))}
-            </Fragment>
-          ))}
-        </div>
+      <div className="flex-1 "></div>
+      <ChatWelcome
+        type={type}
+        name={name}
+      />
+      <div className="flex flex-col-reverse mt-auto">
+        {data?.pages?.map((group, index) => {
+          return <Fragment key={index}>
+            {group.items.map((message: MessageWithMemberWithProfile) => {
+              return <ChatItem
+                key={message.id}
+                id={message.id}
+                currentMember={member}
+                member={message.member}
+                content={message.content}
+                fileUrl= {message.fileUrl}
+                deleted={message.deleted}
+                timestamp={format(new Date(message.createdAt), DATE_FORMAT)}
+                isUpdated={message.updatedAt !== message.createdAt}
+                socketUrl={socketUrl}
+                socketQuery={socketQuery}
+              />;
+            })}
+          </Fragment>;
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ChatMessages
+export default ChatMessages;
